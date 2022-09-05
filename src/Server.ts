cfecea2651706d18190460d13b8a1d0d543a374d
danielhoward <danielnhoward93@gmail.com>
2022-09-05 20:17:05 +0100

    Force silent reditect

--- src/Server.ts
+++ src/Server.ts
@@ -1,10 +1,15 @@
 import express from 'express';
 import http from 'http';
+import path from 'path';
 
 import type {Express} from 'express';
 import type {Server as HttpServer} from 'http';
 import type Base from './base.js';
 import type {UploadStatusMapping} from './types.d.js';
+
+function getPath(url: string): string {
+	return path.join(process.cwd(), url);
+}
 
 export default class Server {
 	app: Express;
@@ -23,13 +28,18 @@ export default class Server {
 	}
 
 	setUpRoutes(): void {
-		this.app.get('/', (req, res) => {
+		// Silently redirect the root to the relevent page for the current status
+		this.app.use('/', (req, res, next) => {
 			const uploadState = this.base.getUploadStatus();
-
 			if (this.uploadStatusMapping[uploadState]) {
-				res.redirect(this.uploadStatusMapping[uploadState]);
+				req.url = this.uploadStatusMapping[uploadState];
 			}
+			next();
 		});
+		this.app.use(
+			Object.values(this.uploadStatusMapping),
+			express.static(getPath('public')),
+		);
 	}
 
 	startServer(): void {
